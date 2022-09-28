b4e00334aa58a1139e0ea40c7ef212cfbb354ddc
MichaelAular <michaelaular@gmail.com>
2022-09-28 10:01:28 +0200

    adding first files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './assets/css/main.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         Hello World
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
